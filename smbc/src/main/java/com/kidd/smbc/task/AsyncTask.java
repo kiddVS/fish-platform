522178c7cbbdf2fdfbb54a1c4ec8344b77346feb
kiddvs <435303131@qq.com>
2021-01-14 00:17:28 +0800

    tg-msg

--- smbc/src/main/java/com/kidd/smbc/task/AsyncTask.java
+++ smbc/src/main/java/com/kidd/smbc/task/AsyncTask.java
@@ -10,12 +10,10 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Enumeration;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
